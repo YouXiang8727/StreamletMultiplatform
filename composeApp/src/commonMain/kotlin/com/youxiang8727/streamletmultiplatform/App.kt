48e0e4e96bccd41ee1e2e9cb89363f2c65b73124
David.hs <david.hs@hsress.com>
2026-02-03 16:59:17 +0800

    feat: Optimize imports

--- composeApp/src/commonMain/kotlin/com/youxiang8727/streamletmultiplatform/App.kt
+++ composeApp/src/commonMain/kotlin/com/youxiang8727/streamletmultiplatform/App.kt
@@ -10,7 +10,11 @@ import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,7 +22,6 @@ import com.youxiang8727.streamletmultiplatform.data.di.databaseModule
 import com.youxiang8727.streamletmultiplatform.data.di.viewModelModule
 import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.KoinApplication
-
 import streamletmultiplatform.composeapp.generated.resources.Res
 import streamletmultiplatform.composeapp.generated.resources.compose_multiplatform
 
