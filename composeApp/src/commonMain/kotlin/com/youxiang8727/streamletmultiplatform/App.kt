6ffccb677fe07dcfa86e8ea83c37314742c7aac4
David.hs <david.hs@hsress.com>
2026-02-12 11:56:13 +0800

    feat: 新增AppTheme; 解決iOS上isSystemInDarkTheme()無法正常運作問題;

--- composeApp/src/commonMain/kotlin/com/youxiang8727/streamletmultiplatform/App.kt
+++ composeApp/src/commonMain/kotlin/com/youxiang8727/streamletmultiplatform/App.kt
@@ -1,12 +1,12 @@
 package com.youxiang8727.streamletmultiplatform
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.youxiang8727.streamletmultiplatform.data.di.databaseModule
 import com.youxiang8727.streamletmultiplatform.data.di.viewModelModule
+import com.youxiang8727.streamletmultiplatform.ui.theme.AppTheme
 import com.youxiang8727.streamletmultiplatform.ui.navigation.MainNavigation
 import org.koin.compose.KoinApplication
 
@@ -21,7 +21,7 @@ fun App() {
             )
         }
     ) {
-        MaterialTheme {
+        AppTheme {
             MainNavigation(
                 modifier = Modifier.fillMaxSize()
             )
---
+++ composeApp/src/commonMain/kotlin/com/youxiang8727/streamletmultiplatform/core/ui/Theme.kt
@@ -0,0 +1,9 @@
+package com.youxiang8727.streamletmultiplatform.core.ui
+
+import androidx.compose.runtime.Composable
+
+/**
+ * REF: https://github.com/JetBrains/compose-multiplatform/issues/3213#issuecomment-1572378546
+ */
+@Composable
+expect fun isSystemInDarkTheme(): Boolean
