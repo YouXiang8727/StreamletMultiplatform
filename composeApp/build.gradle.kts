122ae7ec9862a39bb9e94ca42fa4adfaf7940ef1
David.hs <david.hs@hsress.com>
2026-02-12 11:55:12 +0800

    feat: 導入material-kolor;

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -53,6 +53,7 @@ kotlin {
             implementation(libs.koin.compose)
             implementation(libs.koin.compose.viewmodel)
             implementation(libs.material.icons)
+            implementation(libs.material.kolor)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
